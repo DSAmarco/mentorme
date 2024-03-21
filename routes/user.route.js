9a743524dde1184e8f53dbfaefaffbaee0b808ec
doitmann <cdupree404@gmail.com>
2024-03-21 01:14:40 -0400

    get comet

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 /*import express from "express";
 import { deleteUser } from "../controllers/user.controller";
+//import express from "express";
 
-const router = express.Router();
+//const router = express.Router();
 
-router.get("/test", deleteUser)
+//router.get("/test", )
 
 export default router;*/
+//export default router;
