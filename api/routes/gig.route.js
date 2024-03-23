00bc795039b1dcc7edf30d13d4b36e87742dec02
Jeremy <loseyjeremy@gmail.com>
2024-03-22 23:55:30 -0400

    Added gig functionality in database

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,69 +1,17 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+import express from "express";
+import {
+    createGig,
+    deleteGig,
+    getGig,
+    getGigs
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
-const GigSchema = new Schema({
-    userId: {
-        type: String,
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    desc: {
-        type: String,
-        required: true,
-    },
-    totalStars: {
-        type: Number,
-        default: 0,
-    },
-    starNumber: {
-        type: Number,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    cat: {
-        type: String,
-        required: true,
-    },
-    cover: {
-        type: String,
-        required: true,
-    },
-    img: {
-        type: [String],
-        required: false,
-    },
-    shortTitle: {
-        type: String,
-        required: true,
-    },
-    shortDesc: {
-        type: String,
-        required: true,
-    },
-    deliveryTime: {
-        type: Number,
-        required: true,
-    },
-    revisionNumber: {
-        type: Number,
-        required: true,
-    },
-    features: {
-        type: [String],
-        required: false,
-    },
-    sales: {
-        type: Number,
-        default: 0,
-    },
-}, {
-    timestamps: true
-});
+const router = express.Router();
 
-export default mongoose.model("Gig", GigSchema)
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", getGig);
+router.get("/", getGigs);
+
+export default router;
